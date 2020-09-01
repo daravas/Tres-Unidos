cb91eb2ef42828b6cb633b3b55c00cc060fc8829
Dara <dcsv@cin.ufpe.br>
2020-09-01 01:06:25 -0300

    colocando circulo e quadrado no artboard

--- Tres Unidos/ThirdViewController.swift
+++ Tres Unidos/ThirdViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class ThirdViewController: UIViewController {
     @IBOutlet weak var imageTest: UIImageView!
-   
+    @IBOutlet weak var artboardView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageTest.image = UIImage(named: "teste.jpg") //exemplo
+      //  imageTest.image = UIImage(named: "teste.jpg") //exemplo
         
         // Do any additional setup after loading the view.
     }
@@ -33,11 +34,57 @@ class ThirdViewController: UIViewController {
     }
     
     @IBAction func addTriangle(_ sender: Any) {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 414, height: 539))
+
+                   let img = renderer.image { ctx in
+                       let rectangle = CGRect(x: artboardView.frame.midX, y: artboardView.frame.midY, width: 51, height: 51)
+
+                       ctx.cgContext.setFillColor(UIColor.red.cgColor)
+                       ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+                       ctx.cgContext.setLineWidth(5)
+
+                    ctx.cgContext.add
+                       ctx.cgContext.drawPath(using: .fillStroke)
+                   }
+
+                   let imagem = UIImageView.init(image: img)
+                   artboardView.addSubview(imagem)
        }
     
     @IBAction func addRectangle(_ sender: Any) {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 414, height: 539))
+        
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: artboardView.frame.midX, y: artboardView.frame.midX, width: 51, height: 51)
+
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(5)
+
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .stroke)
+        }
+
+        //artboardView.image = img
+        let imagem = UIImageView.init(image: img)
+        artboardView.addSubview(imagem)
     }
     
     @IBAction func addCircle(_ sender: Any) {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 414, height: 539))
+
+            let img = renderer.image { ctx in
+                let rectangle = CGRect(x: artboardView.frame.midX, y: artboardView.frame.midY, width: 51, height: 51)
+
+                ctx.cgContext.setFillColor(UIColor.red.cgColor)
+                ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+                ctx.cgContext.setLineWidth(5)
+
+                ctx.cgContext.addEllipse(in: rectangle)
+                ctx.cgContext.drawPath(using: .fillStroke)
+            }
+
+            let imagem = UIImageView.init(image: img)
+            artboardView.addSubview(imagem)
     }
 }
